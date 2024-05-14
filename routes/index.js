62d42888d54e947e3a3496e551c577114959970e
WDWIZ <140268841+WDWIZ@users.noreply.github.com>
2024-05-15 08:00:26 +0900

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,12 @@ function indexRouter(sql){
     const router = express.Router();
 
     router.get('/today', async (req, res, next) => {
-        let today = new Date();
+        const utc = curr.getTime() + (curr.getTimezoneOffset() * 60 * 1000);
+
+        const KR_TIME_DIFF = 9 * 60 * 60 * 1000;
+        const kr_curr = new Date(utc + (KR_TIME_DIFF));
+        let today = kr_curr;
+        
         today = moment(today).format("YYYY-MM-DD");
         let msg = await sql.query(`
             SELECT *
